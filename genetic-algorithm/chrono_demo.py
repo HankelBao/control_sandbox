3905e8e32fd81179676033a4412abad71c5c8ab2
HankelBao <hankelbao@outlook.com>
2020-03-17 06:10:51 -0500

    Fix code structure

--- genetic-algorithm/chrono_demo.py
+++ genetic-algorithm/chrono_demo.py
@@ -37,60 +37,65 @@ def main():
     # ------------
     # Create track
     # ------------
-    reversed = random.randint(0,1)
-    track = RandomTrack(x_max=50, y_max=50)
-    track.generateTrack(seed=seed, reversed=reversed)
-    # print('Using seed :: {}'.format(seed))
-    # points = [
-    #     [49.8, 132.9],
-    #     [60.3, 129.3],
-    #     [75.6, 129.0],
-    #     [87.9, 131.7],
-    #     [96.9, 129.6],
-    #     [111.0, 120.0],
-    #     [115.2, 110.7],
-    #     [120.6, 96.9],
-    #     [127.8, 88.5],
-    #     [135.9, 77.4],
-    #     [135.9, 65.1],
-    #     [133.2, 51.3],
-    #     [128.4, 43.2],
-    #     [119.7, 36.3],
-    #     [105.0, 35.7],
-    #     [90.0, 36.3],
-    #     [82.5, 46.2],
-    #     [82.5, 63.6],
-    #     [83.4, 82.2],
-    #     [77.1, 93.9],
-    #     [61.2, 88.5],
-    #     [55.5, 73.5],
-    #     [57.9, 54.6],
-    #     [66.6, 45.0],
-    #     [75.9, 36.3],
-    #     [79.2, 25.5],
-    #     [78.0, 13.2],
-    #     [65.1, 6.0],
-    #     [50.7, 6.0],
-    #     [36.6, 11.7],
-    #     [29.1, 21.3],
-    #     [24.0, 36.9],
-    #     [24.0, 56.1],
-    #     [29.1, 70.8],
-    #     [24.9, 77.7],
-    #     [13.5, 77.7],
-    #     [6.3, 81.6],
-    #     [5.7, 92.7],
-    #     [6.3, 107.7],
-    #     [8.7, 118.2],
-    #     [15.3, 122.7],
-    #     [24.3, 125.4],
-    #     [31.2, 126.0],
-    #     [40.8, 129.6],
-    #     [49.8, 132.9],
-    # ]
+    createRandomTrack = False
+    track = None
 
-    # track = Track(points)
-    # track.generateTrack()
+    if createRandomTrack:
+        reversed = random.randint(0,1)
+        track = RandomTrack(x_max=50, y_max=50)
+        track.generateTrack(seed=seed, reversed=reversed)
+    else:
+        print('Using seed :: {}'.format(seed))
+        points = [
+            [49.8, 132.9],
+            [60.3, 129.3],
+            [75.6, 129.0],
+            [87.9, 131.7],
+            [96.9, 129.6],
+            [111.0, 120.0],
+            [115.2, 110.7],
+            [120.6, 96.9],
+            [127.8, 88.5],
+            [135.9, 77.4],
+            [135.9, 65.1],
+            [133.2, 51.3],
+            [128.4, 43.2],
+            [119.7, 36.3],
+            [105.0, 35.7],
+            [90.0, 36.3],
+            [82.5, 46.2],
+            [82.5, 63.6],
+            [83.4, 82.2],
+            [77.1, 93.9],
+            [61.2, 88.5],
+            [55.5, 73.5],
+            [57.9, 54.6],
+            [66.6, 45.0],
+            [75.9, 36.3],
+            [79.2, 25.5],
+            [78.0, 13.2],
+            [65.1, 6.0],
+            [50.7, 6.0],
+            [36.6, 11.7],
+            [29.1, 21.3],
+            [24.0, 36.9],
+            [24.0, 56.1],
+            [29.1, 70.8],
+            [24.9, 77.7],
+            [13.5, 77.7],
+            [6.3, 81.6],
+            [5.7, 92.7],
+            [6.3, 107.7],
+            [8.7, 118.2],
+            [15.3, 122.7],
+            [24.3, 125.4],
+            [31.2, 126.0],
+            [40.8, 129.6],
+            [49.8, 132.9],
+        ]
+
+        track = Track(points)
+        track.generateTrack()
 
     segmentation = Segmentations(track, k_precision=0.5)
     segmentation.create_segmentations()
@@ -161,7 +166,7 @@ def main():
 
     # Lateral controller (steering)
     lat_controller = PIDLateralController(path)
-    lat_controller.SetGains(Kp=0.4, Ki=0, Kd=0.25)
+    lat_controller.SetGains(Kp=1.9, Ki=0, Kd=0.25)
     lat_controller.SetLookAheadDistance(dist=5)
 
     # Longitudinal controller (throttle and braking)
--- genetic-algorithm/ga.py
+++ genetic-algorithm/ga.py
@@ -8,150 +8,7 @@ import math
 from copy import deepcopy
 import pychrono as chrono
 
-class Segmentations:
-    def __init__(self, track, k_precision=0.800):
-        self.left = []
-        self.right = []
-        self.width = []
-        self.track = track
-        self.size = 0
-        self.precision = k_precision
-
-    def create_segmentations(self):
-        k_sum = 0.0
-
-        right_points = []
-        left_points = []
-        width = []
-        for i in range(self.track.center.length):
-            k_sum += np.abs(self.track.center.k[i])
-            if k_sum >= self.precision:
-                k_sum = 0
-
-                left = self.track.left_waypoints[i]
-                right = self.track.right_waypoints[i]
-                left_points.append(left)
-                right_points.append(right)
-
-                width.append(np.sqrt(np.square(left[0]-right[0])+np.square(left[1]-right[1])))
-                self.size += 1
-
-        self.left = Path(left_points, closed=True, raw_mode=True)
-        self.right = Path(right_points, closed=True, raw_mode=True)
-        self.width = np.array(width)
-
-    def get_point(self, index, a):
-        x1 = self.left.x[index]
-        y1 = self.left.y[index]
-        x2 = self.right.x[index]
-        y2 = self.right.y[index]
-        x = x1 + (x2-x1)/self.width[index]*a
-        y = y1 + (y2-y1)/self.width[index]*a
-        return [x, y]
-
-    def plot(self):
-        for i in range(self.size):
-            plt.plot([self.left.x[i], self.right.x[i]], [self.left.y[i], self.right.y[i]], "g-")
-
-class Cell:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.fCost = 0.0
-
-class RAStar:
-    def __init__(self, segmentation, neightbors_ratio=0.2, divisions=10):
-        # f(x) = g(x) + h(x)
-        self.segmentation = segmentation
-        self.neightbors = int(divisions*neightbors_ratio)
-        self.divisions = divisions
-        self.width = self.segmentation.size
-        self.height = self.divisions
-
-        self.t_break = 1+1/(self.width + self.height)
-        self.g_score = np.full([self.width, self.height], np.inf)
-        self.division_val = 1/divisions
-
-    def find_a(self, start_a):
-        start_y = int(start_a*self.divisions)
-        self.start_y = start_y
-
-        self.g_score[0][start_y] = 0
-        currentCell = Cell(0, start_y)
-        currentCell.fCost = self.g_score[0][start_y] + self.t_break*self.calc_heuristic(0, start_y)
-        opl = [ currentCell ]
-
-        while (np.min(self.g_score[self.width-1]) == np.inf):
-            cof_cell = heapq.nsmallest(1, opl, key=lambda k: k.fCost)[0]
-            opl.remove(cof_cell)
-
-            for cell in self.get_neighbours(cof_cell):
-                if self.g_score[cell.x][cell.y] == np.inf:
-                    self.g_score[cell.x][cell.y] = self.calc_gcost(cof_cell.x, cof_cell.y, cell.x, cell.y)
-                    cell.fCost = self.g_score[cell.x][cell.y] + self.t_break*self.calc_heuristic(cell.x, cell.y)
-
-                    if not any((cell.x == existing_cell.x and cell.y == existing_cell.y) for existing_cell in opl):
-                        opl.append(cell)
-
-            # print(self.g_score)
-
-        currentCell = Cell(0, start_y)
-        a = [start_y]
-        while currentCell.x != self.width-1:
-            neighbours = self.get_neighbours(currentCell)
-            best_next = heapq.nsmallest(1, neighbours, key=lambda k:self.g_score[k.x][k.y])[0]
-            currentCell = best_next
-            a.append(currentCell.y)
-
-        a = np.array(a)
-        return a/self.divisions
-
-    def get_neighbours(self, cell):
-        y_min = max(0, cell.y-self.neightbors)
-        y_max = min(self.divisions, cell.y+self.neightbors)
-
-        neighbours = []
-        for i in range(y_min, y_max):
-            neighbours.append(Cell(cell.x+1, i))
-        return neighbours
-
-    def calc_gcost(self, x1, y1, x2, y2):
-        (px1, py1) = self.segmentation.get_point(x1, y1*self.division_val)
-        (px2, py2) = self.segmentation.get_point(x2, y2*self.division_val)
-        return self.g_score[x1][y1] + np.sqrt(np.square(px2-px1)+np.square(py2-py1))
-
-    def calc_heuristic(self, x, y):
-        # return np.abs(self.width-x) + np.abs(self.start_y-y)
-        return self.g_score[x][y]/x*(self.width-x)
-
-class TrackPath(Path):
-    def __init__(self, segmentation: Segmentations, a):
-        self.size = segmentation.size
-        self.a = a
-
-        points = []
-        for i in range(segmentation.size):
-            points.append(segmentation.get_point(i, a[i]))
-        super().__init__(points, closed=True, raw_mode=True)
-
-        self.adaptability = -np.sum(self.t)
-        self.point_adapt = self.v_max
-
-    def plot_path(self, show=False, color="r^-"):
-        self.plot(show=show, color=color)
-
-    def generate_final_path(self):
-        points = []
-        for i in range(self.size):
-            points.append([self.x[i], self.y[i]])
-
-        final_path = Path(points, num_points=500)
-        points = []
-        for i in range(final_path.length):
-            points.append([final_path.x[i], final_path.y[i]])
-        final_track_path = Path(points, raw_mode=True)
-        self.final_path = final_track_path
-
+from segmentation import Segmentations, TrackPath
 
 class GAConfig():
     def __init__(self, segmentation):
@@ -160,8 +17,7 @@ class GAConfig():
         self.a_min = np.full(segmentation.size, 0.1)
         self.a_max = np.full(segmentation.size, 0.9)
 
-        self.preserve_a = False
-        self.population = int(segmentation.size*8)
+        self.population = int(segmentation.size)
 
         self.c = 0.4
         self.gc = 0.4
@@ -202,9 +58,6 @@ class GAPathGenerator:
             self.selection_p.append(self.path[i].adaptability/adaptability_sum)
 
     def init_generation(self):
-        if self.config.preserve_a:
-            self.path.append(TrackPath(self.segmentation, self.config.initial_a))
-
         for _ in range(self.config.population-1):
             a = np.random.uniform(self.a_min, self.a_max)
             self.path.append(TrackPath(self.segmentation, a))
